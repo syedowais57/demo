4bba85bd9619ba949f6f9f91d6881172372a6e5a
syedowais57 <owaisquadri8575@gmail.com>
2025-12-03 17:02:39 +0530

    Add logging and use extra_utils

--- src/main.py
+++ src/main.py
@@ -1,4 +1,6 @@
 from .helpers import greet
+from .extra_utils import bye   # NEW IMPORT TO TEST DEPENDENCIES
 
 def handler(name: str) -> None:
-    print(greet(name))
+    print(greet(name).upper())
+    print(bye(name))
