137cae4f795086f45f79c68fcce0a18494ce4c74
syedowais57 <owaisquadri8575@gmail.com>
2025-12-04 12:56:19 +0530

    Test PR: build should pass

--- src/main.py
+++ src/main.py
@@ -1,12 +1,8 @@
 from .helpers import greet
 from .dependency_demo import format_user_message
 
+t
 
 def handler(name: str) -> None:
-    # Slight change so there is a new diff
-    greeting = greet(name)
-    print(greeting)
+    print(greet(name))
 
-    # This will call the buggy helper
-    message = format_user_message(name)
-    print(message)
